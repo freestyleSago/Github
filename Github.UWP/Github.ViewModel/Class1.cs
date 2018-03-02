efb2f1e9d3c49927f10bdc50db98f7bff97923db
Sago <freestyle.sago@outlook.com>
2018-03-02 21:05:22 +0800

    初始化

---
+++ Github.UWP/Github.ViewModel/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Github.ViewModel
+{
+    public class Class1
+    {
+    }
+}
